a84375f38fdb79d476fffdc9b67ea6011ec98b5a
Elena Lisichenok <111569149+lena-lis@users.noreply.github.com>
2023-10-15 11:04:58 +0300

    feat & fix(menu-toggle): добавляет стили состояний, добавляет возможность закрытия меню по клику на оверлей и по нажатию ESc

--- source/js/modules/menu-toggle.js
+++ source/js/modules/menu-toggle.js
@@ -1,9 +1,26 @@
 import '../utils/scroll-lock';
+import {isEscapeKey} from '../utils/utils';
 
 const mainHeaderElement = document.querySelector('[data-main-header]');
-// const navMainElement = document.querySelector('[data-nav-main]');
+const navMainElement = document.querySelector('[data-nav-main]');
 const navToggleButton = document.querySelector('[data-nav-toggle]');
 const navMenuElement = document.querySelector('[data-nav-menu]');
+
+function onDocumentKeydown(evt) {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeMenu();
+  }
+}
+
+function onOverlayClick(evt) {
+  const menuTargeted = evt.target === navMainElement || navMainElement.contains(evt.target);
+  const navMenuElementIsOpen = navMenuElement.classList.contains('is-open');
+
+  if (!menuTargeted && navMenuElementIsOpen) {
+    closeMenu();
+  }
+}
 
 function closeMenu() {
   mainHeaderElement.classList.remove('is-active');
@@ -11,6 +28,8 @@ function closeMenu() {
   navMenuElement.classList.remove('is-open');
   window.scrollLock.enableScrolling();
   navToggleButton.removeEventListener('click', closeMenu);
+  document.removeEventListener('click', onOverlayClick);
+  document.removeEventListener('keydown', onDocumentKeydown);
 }
 
 const openMenu = () => {
@@ -23,6 +42,8 @@ const openMenu = () => {
   navMenuElement.classList.add('is-open');
   window.scrollLock.disableScrolling();
   navToggleButton.addEventListener('click', closeMenu);
+  document.addEventListener('click', onOverlayClick);
+  document.addEventListener('keydown', onDocumentKeydown);
 };
 
 const onNavToggleButtonClick = () => {
